6b98c3b1bdb800ee14b95ac561d74ad64fead34d
yamauchi <toshikazu.yamauchi@jig-saw.com>
2016-11-24 12:40:30 +0900

    詳細、結果、投票のビューページを追加

--- tutorial/polls/views.py
+++ tutorial/polls/views.py
@@ -4,3 +4,12 @@ from django.http import HttpResponse
 def index(request):
     return HttpResponse("ビューのインデックス関数のレスポンス")
 
+def detail(request, question_id):
+    return HttpResponse("あなたは %s 番のQuestionを見ています" % question_id)
+
+def results(request, question_id):
+    response = "あなたは %s 番のQuestionの結果を見てる"
+    return HttpResponse(response % question_id)
+
+def vote(request, question_id):
+    return HttpResponse("あなたは %s 番目のQuestionに投票する" % question_id)
