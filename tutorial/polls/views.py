3f9367ee8da5624b2626c77ae24fac0acc8dfda8
yamauchi <toshikazu.yamauchi@jig-saw.com>
2016-11-24 18:40:26 +0900

    投票の仕組みを追加（バグ有り）

--- tutorial/polls/views.py
+++ tutorial/polls/views.py
@@ -1,8 +1,9 @@
 from django.shortcuts import render,get_object_or_404
-from django.http import HttpResponse,Http404
+from django.http import HttpResponseRedirect, HttpResponse
 from django.template import loader
+from django.urls import reverse
 
-from .models import Question
+from .models import Choice, Question
 
 def index(request):
     latest_question_list = Question.objects.order_by('-pub_date')[:5]
@@ -16,9 +17,22 @@ def detail(request, question_id):
     return render(request,'polls/detail.html' ,{ 'question':question })
 
 def results(request, question_id):
-    response = "あなたは %s 番のQuestionの結果を見てる"
-    return HttpResponse(response % question_id)
+    question = get_object_or_404(Question, pk=question_id)
+    return render(request, 'polls/results.html', {'question':question})
+
 
 def vote(request, question_id):
-    return HttpResponse("あなたは %s 番目のQuestionに投票する" % question_id)
+    question = get_object_or_404(Question, pk=question_id)
+    try:
+        selected_choice = question.choice_set.get(pk=request.POST['choice'])
+    except (KeyError, Choice.DoesNotExist):
+        # Redisplay the question voting form.
+        return render(request, 'polls/detail.html', {
+            'question': question,
+            'error_message': "どれかを選択してくださいな！",
+            })
+    else:
+        selected_choice.votes += 1
+        selected_choice.save()
+        return HttpResponseRedirect(reverse('polls:results', args=(question.id,)))
 
